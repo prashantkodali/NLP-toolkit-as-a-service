3e25eae34254b8ac6e8785f6c59388fc47201f8c
Ananya Mukherjee <36190805+AnanyaCoder@users.noreply.github.com>
2020-04-14 23:54:02 +0530

    Add files via upload

--- NER/WebNER.py
+++ NER/WebNER.py
@@ -5,19 +5,20 @@ Created on Fri Apr 10 18:55:53 2020
 @author: Sushom-Dell
 """
 
-import spacy
+from bs4 import BeautifulSoup
+import requests
+import re
 from spacy import displacy
-from collections import Counter
-from spacy.lang.en import English
 import en_core_web_sm
+from flask import Flask, jsonify, request,render_template,redirect, url_for
 
-#import webbrowser
+
 nlp = en_core_web_sm.load()
 
-doc = nlp('European authorities fined Google a record $5.1 billion on Wednesday for abusing its power in the mobile phone market and ordered the company to alter its practices')
-tags = [(X.text, X.label_) for X in doc.ents]
+#doc = nlp('European authorities fined Google a record $5.1 billion on Wednesday for abusing its power in the mobile phone market and ordered the company to alter its practices')
+#tags = [(X.text, X.label_) for X in doc.ents]
 
-from flask import Flask, jsonify, request,render_template,redirect, url_for
+
 app = Flask(__name__)
 app.config["DEBUG"] = True
  
@@ -27,24 +28,40 @@ def index():
  
 @app.route("/ner/")
 def NER():
-    return render_template("ananya.html")
+    return render_template("ner.html")
 
 @app.route("/getNER/", methods=['POST','GET'])
 def form():
     
     if(str(request.form["url_ip"])):
         tags = getNerTagsFromURl(str(request.form["url_ip"]))
+        
     elif(str(request.form["txt_ip"])):
         tags = getNerTags(str(request.form["txt_ip"]))
     
-    #return tags
-    return render_template("ananya.html", tags=tags)
+    return render_template("ner.html", tags=tags)
 
 def getNerTagsFromURl(url):
     #retrieve article from url
+    ny_bb = url_to_string(url)
+    #print("hi",ny_bb)
+    article = nlp(ny_bb)
+    
+    X = displacy.render(article, jupyter=None, style='ent')
+    print(len(article.ents))
+   #sentences = [x for x in article.sents]
+   # print(str(sentences[20]))
     #get ner tags
     print(url)
-    return 1
+    return X
+
+def url_to_string(url):
+    res = requests.get(url)
+    html = res.text
+    soup = BeautifulSoup(html, 'html5lib')
+    for script in soup(["script", "style", 'aside']):
+        script.extract()
+    return " ".join(re.split(r'[\n\t]+', soup.get_text()))
 
 def getNerTags(inputText): 
     print("text:", inputText)
