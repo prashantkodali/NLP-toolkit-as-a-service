8b235ab9d514ab5bf867a2797252fdc8832860f1
deciphyre <saumitra.yadav@research.iiit.ac.in>
2020-04-19 16:18:14 +0530

    removed non essential comments

--- MT/server.py
+++ MT/server.py
@@ -4,8 +4,6 @@ import configargparse
 from flask import Flask, jsonify, request,render_template,redirect, url_for
 from onmt.translate import TranslationServer, ServerModelError
 import json
-from nltk import sent_tokenize
-from nltk.tokenize import word_tokenize
 import re
 import os
 from nltk.tokenize import word_tokenize
@@ -25,20 +23,16 @@ def start(config_file,
     translation_server = TranslationServer()
     translation_server.start(config_file)
     
-    
     @app.route('/translate', methods=['POST'])
     def translate():
         inputs = request.get_json(force=True)
-        #inputs=[request.form.to_dict()]
         print(inputs)
         originalInput = inputs[0]['src']
         sentences=sent_tokenize(inputs[0]['src'])
-        #sentences=[]
         if int(inputs[0]['id'])==100:
           tok_src=[" ".join(word_tokenize(x.lower())) for x in sentences]
         else:
           tok_src=[" ".join(word_tokenize(x)) for x in sentences]
-        print ('input recieved')
         inputs[0]['id']=int(inputs[0]['id'])
         tok_tgt={}
         for sent in tok_src:
@@ -55,7 +49,6 @@ def start(config_file,
             except ServerModelError as e:
                   out['error'] = str(e)
                   out['status'] = STATUS_ERROR
-            #print(out[0]['tgt'])
             print ('translation done')
             if 'src' not in tok_tgt:
                   tok_tgt['src']=[out[0]['src']]
@@ -63,35 +56,27 @@ def start(config_file,
             else:
                   tok_tgt['src'].append(out[0]['src'])
                   tok_tgt['tgt'].append(out[0]['tgt'])
-        tok_tgt['src']=originalInput#" ".join(" ".join(tok_tgt['src']).strip().split())
+        tok_tgt['src']=originalInput
         tok_tgt['tgt']="\n".join(tok_tgt['tgt'])
-        #tok_tgt['ipVisibleToOutside']=ip
-        #################return jsonify(translation[0])
-        #################return redirect(url_for(home, value=out[0]))
-        #return render_template("index.html", value=tok_tgt)
         return jsonify(tok_tgt)
-        #return out[0]['tgt']
     
     @app.route('/')
     def home():
-        #if request.args.get('value'):
-        #        out={'src':value['src'],'tgt':value['tgt']}
-        #else:
-        out={'src':'','tgt':''}#, 'ipVisibleToOutside':ip}
-        return render_template('index.html',value=out)
+        out={'src':'','tgt':''}
+        return jsonify(out)
+
     app.run(debug=debug, host=host, port=port, use_reloader=False,
             threaded=True)
 
-
 def _get_parser():
     parser = configargparse.ArgumentParser(
-        config_file_parser_class=configargparse.YAMLConfigFileParser,
-        description="OpenNMT-py REST Server")
+            config_file_parser_class=configargparse.YAMLConfigFileParser,
+        description="NMT RestAPI Server")
     parser.add_argument("--ip", type=str, default="0.0.0.0")
     parser.add_argument("--port", type=int, default="5000")
     parser.add_argument("--url_root", type=str, default="/translator")
     parser.add_argument("--debug", "-d", action="store_true")
-    parser.add_argument("--config", "-c", type=str,default="./available_models/conf.json")
+    parser.add_argument("--config", "-c", type=str,default="./available_models/transConf.json")
     
     return parser
 
@@ -100,5 +85,4 @@ if __name__ == '__main__':
     parser = _get_parser()
     args = parser.parse_args()
     ip = args.ip
-    start(args.config, url_root=args.url_root, host=args.ip, port=args.port,
-          debug=args.debug)
+    start(args.config, url_root=args.url_root, host=args.ip, port=args.port,debug=args.debug)
