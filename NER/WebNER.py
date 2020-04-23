3639eee40e6015a691714ac0c779a219600e4d44
Ananya Mukherjee <36190805+AnanyaCoder@users.noreply.github.com>
2020-04-23 10:58:44 +0530

    Add files via upload

--- NER/WebNER.py
+++ NER/WebNER.py
@@ -1,12 +1,8 @@
 # -*- coding: utf-8 -*-
 """
 Created on Fri Apr 10 18:55:53 2020
-@author: Ananya Mukherjee
-#***********************************************************************************************
-#Description : Server Side Program
-#Model : SpaCy’s named entity recognition model is trained on OntoNotes5 corpus. 
-#      : Assigns context-specific token vectors, POS tags, dependency parse and named entities.
-#***********************************************************************************************
+
+@author: Sushom-Dell
 """
 from collections import Counter
 from bs4 import BeautifulSoup
@@ -18,10 +14,18 @@ import en_core_web_sm
 from flask import Flask, jsonify, request,render_template,redirect, url_for
 import requests
 
-#Loads the model trained on CNN 
 nlp = en_core_web_sm.load()
 
 app = Flask(__name__)
+#app.config["DEBUG"] = True
+ 
+@app.route("/")
+def index():
+    return "<h1>To find NER, append /ner/</h1>"
+ 
+@app.route("/ner/")
+def NER():
+    return render_template("ner.html")
 
 @app.route("/getNER", methods=['POST','GET'])
 def form():
@@ -37,17 +41,12 @@ def form():
 
     return jsonify({'output_text':tags})
 
+
 def getNerTagsFromURl(url):
-    ny_bb = url_to_string(url)
-    
+    ny_bb = url_to_string(url) 
     article = nlp(ny_bb)
-    labels = [x.label_ for x in article.ents]
-    cnt_dict = Counter(labels) #print the cardinality 
-    
-    X = displacy.render(article, jupyter=None, style='ent')
-    print(len(article.ents))
-    print(url)
-    return X
+    #X = displacy.render(article, jupyter=None, style='ent')
+    return renderNerOutputFromText(article)
 
 def url_to_string(url):
     res = requests.get(url)
@@ -60,7 +59,12 @@ def url_to_string(url):
 def getNerTags(inputText): 
     doc1 = nlp(inputText)
     sentences = [x.text for x in doc1.sents]
-    X = displacy.render(nlp(str(sentences)), jupyter=None, style='ent')
-    return X
+    #X = displacy.render(nlp(str(sentences)), jupyter=None, style='ent')
+    return renderNerOutputFromText(nlp(str(sentences)))
 	
+def renderNerOutputFromText(text):
+    return displacy.render(text, jupyter=None, style='ent')
+
+
+
 app.run(host='0.0.0.0', port = 8080, debug=True)
