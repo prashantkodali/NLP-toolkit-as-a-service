39ca877b3261d6603404d2b6b1adb5ab27b10c26
Neha Motlani <nehamotlani@Nehas-MacBook-Pro.local>
2020-04-27 19:25:00 +0530

    error handling added

--- Tokenizer/HuggingFaceTokenizer.py
+++ Tokenizer/HuggingFaceTokenizer.py
@@ -1,6 +1,5 @@
 from tokenizers import ByteLevelBPETokenizer, SentencePieceBPETokenizer, BertWordPieceTokenizer
 
-sentence = "We need small heroes so that big heroes can shine"
 
 # Bert vocabularies
 bertBaseCased = "bert-base-cased-vocab.txt"
@@ -10,10 +9,15 @@ bertLargeUncased = "bert-large-uncased-vocab.txt"
 # GPT-2 vocabularies
 gpt2Vocab = "gpt2-vocab.json"
 gpt2LargeVocab = "gpt2-large-vocab.json"
-# Instantiate a Bert tokenizers
-WordPiece = BertWordPieceTokenizer(bertLargeUncased)
-WordPieceEncoder = WordPiece.encode(sentence)
-# Print the ids, tokens and offsets
-print(WordPieceEncoder.ids)
-print(WordPieceEncoder.tokens)
-print(WordPieceEncoder.offsets)
+
+def tokenize(sentence):
+	# Instantiate a Bert tokenizers
+	WordPiece = BertWordPieceTokenizer(bertLargeUncased)
+	WordPieceEncoder = WordPiece.encode(sentence)
+	# Print the ids, tokens and offsets
+	print(WordPieceEncoder.ids)
+	print(WordPieceEncoder.tokens)
+	print(WordPieceEncoder.offsets)
+
+sentence = "We need small heroes so that big heroes can shine"
+
